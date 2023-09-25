48731a7ede1188c2daf7eea1dca5ac96230ff73d
jrodva <jr.rodriguez@crowdfarming.com>
2023-09-25 13:48:47 +0200

    chore(initial-config): add database connection

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountsModule } from '../accounts/accounts.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
+  imports: [AccountsModule, MongooseModule.forRoot(process.env.DB_URI)],
   providers: [AppService],
 })
 export class AppModule {}
