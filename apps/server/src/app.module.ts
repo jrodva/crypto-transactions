039baaeaf5905be7e7538aa031f01c715ecbd79e
jrodva <jr.rodriguez@crowdfarming.com>
2023-09-27 23:52:00 +0200

    feat(part2): change app module to expose AppGateway

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { AccountsModule } from './modules/accounts/accounts.module';
 import { BtcRateModule } from './modules/btc-rate/btc-rate.module';
+import { AppGateway } from './app.gateway';
 
 @Module({
+  imports: [AccountsModule, BtcRateModule, MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [],
-  imports: [MongooseModule.forRoot(process.env.DB_URI), AccountsModule, BtcRateModule],
-  providers: [],
+  providers: [AppGateway],
 })
 export class AppModule {}
