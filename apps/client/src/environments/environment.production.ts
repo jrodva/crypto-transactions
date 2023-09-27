53e5808419f6b36375bf772c8715745fc3329140
jrodva <jr.rodriguez@crowdfarming.com>
2023-09-27 23:57:29 +0200

    refactor(part2): remove unnecessary environment variable

--- apps/client/src/environments/environment.production.ts
+++ apps/client/src/environments/environment.production.ts
@@ -2,7 +2,6 @@ export const environment = {
   API_URL: {
     BASE: '[INTRODUCE YOUR OWN PRODUCTION API URL]',
     ACCOUNTS: 'accounts',
-    BTC_RATE: 'btc-rate',
   },
   WS_URL: '[INTRODUCE YOUR OWN PRODUCTION WS URL]',
   production: true,
--- apps/client/src/environments/environment.ts
+++ apps/client/src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
   API_URL: {
     BASE: 'http://localhost:5000/api',
     ACCOUNTS: 'accounts',
-    BTC_RATE: 'btc-rate',
   },
   WS_URL: 'http://localhost:83',
   production: false,
