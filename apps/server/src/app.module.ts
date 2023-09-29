5f15d3634c36d466efdd0e58ad50ef03f63db648
jrodva <jr.rodriguez@crowdfarming.com>
2023-09-29 16:30:50 +0200

    feat(part4): add basic resource for transactions

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AccountsModule } from './modules/accounts/accounts.module';
 import { BtcRateModule } from './modules/btc-rate/btc-rate.module';
 import { AppGateway } from './app.gateway';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
-  imports: [AccountsModule, BtcRateModule, MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [AccountsModule, BtcRateModule, MongooseModule.forRoot(process.env.DB_URI), TransactionsModule],
   controllers: [],
   providers: [AppGateway],
 })
